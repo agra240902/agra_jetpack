0dd39588d0d26e7386bf00d0d391640907bbb13a
agra240902 <agranibrasul@gmail.com>
2024-05-13 21:49:18 +0700

    jetpack agra

---
+++ app/src/main/java/com/example/submissioncompose/model/PlayerData.kt
@@ -0,0 +1,109 @@
+package com.example.submissioncompose.model
+
+import com.example.submissioncompose.R
+
+object PlayerData {
+    val dummyPlayer = listOf(
+        Player(
+            id = 0,
+            name = "Aboy",
+            team = "Geek Fam",
+            image = R.drawable.aboy,
+            description = "Aboy sendiri adalah seorang midlaner yang mencuri perhatian dalam Mobile Legends: Bang Bang Professional League Indonesia Season 12. Bernama lengkap Valent Agriansyah Putra, Aboy mendapatkan nama panggilan dari teman-temannya di sekolah yang sering memanggilnya dengan sebutan Aboy. Memiliki Chou sebagai hero favorit, Aboy memulai karier di MLBB Esports pada pertengahan 2022 bersama Bless Esports, berhasil membawa timnya meraih peringkat kedua dalam turnamen Super Esport Series Mobile Legends.Keberhasilannya di turnamen tersebut menarik perhatian Geek Fam, dan Aboy direkrut oleh tim. Awalnya dimasukkan ke dalam roster Geek Jr untuk berkompetisi di Mobile Legends: Bang Bang Development League Indonesia Season 6, Aboy berhasil membawa Geek Jr meraih juara ketiga, dan kemudian dipromosikan untuk bermain di MPL Indonesia Season 7.",
+            positional = "All Role",
+            rating = 4.5,
+            active = "Aktif"
+        ),
+        Player(
+            id = 1,
+            name = "Clay",
+            team = "Rex Regium Qeon",
+            image = R.drawable.clay,
+            description = "Clay resmi bergabung dengan tim RRQ Hoshi sebagai roster untuk gelaran MPL Season 8 nanti. Kabar ini disambut antusias oleh para penikmat esports. Clay dinilai bisa semakin memperkuat tim Raja dari Segala Raja itu.Sebagai pro player muda, Clay sudah terjun ke kompetitif scene dengan ikutan turnamen-turnamen sejak masih SMA.Clay dulu pernah dikenal sebagai Top Global Harith. Sudah tidak perlu diragukan lagi skill Clay ketika menggunakan hero tersebut.",
+            positional = "Midlane",
+            rating = 4.2,
+            active = "aktif"
+        ),
+        Player(
+            id = 2,
+            name = "Emperor",
+            team = "No Team",
+            image = R.drawable.emperor,
+            description = "Emperor nyatanya pertama kali masuk ke industri esports dengan bergabung di tim Bigetron. Dari situlah, karirnya di dunia esports baru dimulai.Dia saat itu menampilkan permainan yang memukau,berkat performanya itu, Emperor akhirnya dilirik oleh tim Macan Putih, EVOS Esports. Pada saat itulah, kemampuan Emperor dibuktikan dengan sangat baik, sampai-sampai merebut beragam prestasi kala itu.",
+            positional = "Junggler",
+            rating = 4.0,
+            active = "Pensiun"
+        ),
+        Player(
+            id = 3,
+            name = "Lemon",
+            team = "Rex Regium Qeon",
+            image = R.drawable.lemon,
+            description = "Lemon mendapat julukan “Alien” berkat bakatnya dalam menggunakan hero hero non-meta saat turnamen. Jadi julukan ini merupakan pujian dari komunitas Mobile Legends atas skill Lemon. Layaknya julukan “Alien” untuk Messi di sepak bola.",
+            positional = "All Role",
+            rating = 4.4,
+            active = "Aktif"
+        ),
+        Player(
+            id = 4,
+            name = "Luminaire",
+            team = "No Team",
+            image = R.drawable.luminer,
+            description = "Sebagai salah satu pemain veteran, kepiawaian Luminaire sudah tidak perlu diragukan. Ia sudah memberi total 2 (dua) gelar MPL ID bagi EVOS, serta 1 gelar MLBB World Championship (M1). Tak heran, sekarang masih banyak fans yang berharap dia kembali bermain.",
+            positional = "Roamer",
+            rating = 4.5,
+            active = "Pensiun"
+        ),
+        Player(
+            id = 5,
+            name = "Renbo",
+            team = "Rex Regium Qeon",
+            image = R.drawable.renbo,
+            description = "Karier Renbo di dunia esports MLBB berawal dari tim Lord. Setelah dari Lord, ia kemudian pindah ke Saints Indo. Namun ia belum diberi kesempatan untuk terjun ke panggung kompetitif.Dari sana, ia kemudian bergabung dengan BOOM ID. Namanya sebagai pro player akhirnya benar-benar tersalurkan ketika ia bergabung dengan tim Victim. Punya skill dan prestasi, ia akhirnya dilirik oleh Bigetron untuk bermain di MPL.Sebelum rehat Renbo sempat memperkuat team RRQ dan sayangnya dia belum menemukan performa terbaiknya.",
+            positional = "All Role",
+            rating = 4.2,
+            active = "Rehat"
+        ),
+        Player(
+            id = 6,
+            name = "Sanji",
+            team = "Echo PH",
+            image = R.drawable.sanji,
+            description = "Sanji baru memulai debutnya di tim ECHO PH pada pertengahan tahun 2022. Meski baru bergabung, ia berhasil membawa timnya menjuarai M4 World Championship.Debut pertengahan tahun 2022, Sanji sudah menorehkan dua prestasi yaitu juara 2 MPL Philippines Season 10 dan juara 1 M4 World Championship.",
+            positional = "Midlaner",
+            rating = 4.7,
+            active = "Aktif"
+        ),
+        Player(
+            id = 7,
+            name = "Sanz",
+            team = "Onic ID",
+            image = R.drawable.sanz,
+            description = "ONIC Sanz adalah salah satu pemain paling istimewa saat ini. Ia dianggap mid laner paling kuat, bahkan pemain terbaik yang ada sekarang.Mekanik tingkat tinggi, makro luar biasa, dan level permainan yang begitu konsisten membuat ONIC Sanz begitu disegani.Sekarang hampir semua orang mengatakan bahwa tak ada mid laner yang lebih baik dari ONIC Sanz.",
+            positional = "Midlaner",
+            rating = 5.0,
+            active = "Aktif"
+        ),
+        Player(
+            id = 8,
+            name = "Udil",
+            team = "Homebois",
+            image = R.drawable.udil,
+            description = " Udil  menjadi salah satu pemain handal dalam menggunakan hero Mage seperti Lunox, Luo yi, Pharsa, Selena, Kagura, terutama Valir dan masih banyak lagi.Bersama alter ego dia mempersembahkan juara MPLI season 2 dan runner-up MPL ID season 6 . Dan bersama onic dia mempersembahkan juara MPL ID,MSC,dan PIALA PRESIDEN.",
+            positional = "Midlaner",
+            rating = 4.5,
+            active = "Aktif"
+        ),
+        Player(
+            id = 9,
+            name = "Wann",
+            team = "GPX",
+            image = R.drawable.wan
+            ,
+            description = "Wann mulai dikenal luas setelah EVOS Legends meraih juara di berbagai turnamen besar, seperti M1 dan beberapa musim MPL-ID. peran Wann sangat penting dalam mengatur strategi dan melakukan serangan taktis untuk membawa timnya meraih kemenangan.",
+            positional = "Jungler",
+            rating = 4.3,
+            active = "Rehat"
+        )
+    )
+}
